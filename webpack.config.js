e6e1ec106d10774080306bf8632f5373070e920f
ShimmyLikeJamie <oppel.jamie@gmail.com>
2021-07-24 16:28:41 -0700

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+
+// Generated using webpack-cli http://github.com/webpack-cli
+const path = require('path');
+
+module.exports = {
+    mode: 'development',
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+    plugins: [
+        // Add your plugins here
+        // Learn more obout plugins from https://webpack.js.org/configuration/plugins/
+    ],
+    module: {
+        rules: [  
+            // Add your rules for custom modules here
+            // Learn more about loaders from https://webpack.js.org/loaders/
+            {
+                test: /\.m?js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            }
+        ],
+    },
+};
